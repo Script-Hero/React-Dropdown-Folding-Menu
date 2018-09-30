7923c36ab96977d2378821b3eadd2bf35bdbb99d
Neil <neilaylor@hotmail.com>
2018-09-30 18:39:48 -0500

    Finished transitions for folding-item

--- src/components/folding-item/folding-item.css.js
+++ src/components/folding-item/folding-item.css.js
@@ -1,14 +1,25 @@
-var FoldingItem = {
-  width : '300px',
-  height : '30px',
+var folding_item = {
+  width : '0px', // Transitions to 300px
+  height : '0px', // Transitions to 30px
   border : '2px solid black',
   textAlign : 'center',
   display : 'table',
-  visibility : 'hidden'
+  visibility : 'hidden',
+  transition : 'height .2s, width .1s',
+
+  marginLeft : '40px',
+  marginTop : '10px',
+  borderTopRightRadius : '10px',
+  borderBottomRightRadius : '10px',
+  borderTopLeftRadius : '3px',
+  borderBottomLeftRadius : '3px'
 }
 
 var folding_item_text = {
   display : 'table-cell',
-  verticalAlign : 'middle'
+  verticalAlign : 'middle',
+  fontSize : '0px', // Transitions to 12 pixels
+  opacity : '0', // Transitions to 1
+  transition : 'opacity 1s'
 }
-export default {FoldingItem, folding_item_text}
+export default {folding_item, folding_item_text}
--- src/components/folding-item/folding-item.js
+++ src/components/folding-item/folding-item.js
@@ -10,30 +10,68 @@ export default class FoldingItem extends React.Component
   constructor(props)
   {
     super(props);
+    this.state = {folding_item_style : copy_json(styles.folding_item), folding_item_text_style : copy_json(styles.folding_item_text)};
 
-    console.log(props)
+    this.updateStyles = this.updateStyles.bind(this);
+  }
 
-    this.state = {style : copy_json(styles.FoldingItem)};
+  componentDidUpdate(prevProps)
+  {
+    console.log(this.state)
+    setTimeout(function(){
+      var folding_item_text_style = copy_json(this.state.folding_item_text_style);
+      if(this.state.folding_item_text_style.fontSize == '18px')
+      {
+        folding_item_text_style.fontSize = '0px';
+        folding_item_text_style.opacity = '0';
+      }else if (this.state.folding_item_text_style.fontSize == '0px') {
+        folding_item_text_style.fontSize = '18px';
+        folding_item_text_style.opacity = '1';
+      }
+
+
+      if(prevProps.visible !== this.props.visible){
+        this.setState({folding_item_text_style : folding_item_text_style})
+      }
+    }.bind(this), 200)
+  }
+
+  updateStyles()
+  {
+    var folding_item_style = copy_json(this.state.folding_item_style);
+    var folding_item_text_style = copy_json(this.state.folding_item_text_style);
+
+    if(this.props.visible === true)
+    {
+      folding_item_style.visibility = 'visible';
+      folding_item_style.height = '30px';
+      folding_item_style.width = '300px'
+
+    } else if (this.props.visible === false)
+    {
+      folding_item_style.visibility = 'hidden'
+      folding_item_style.height = '0px'
+      folding_item_style.width = '0px'
+
+    }
+
+
+    if(JSON.stringify(folding_item_style) !== JSON.stringify(this.state.folding_item_style)){
+      this.setState({folding_item_style : folding_item_style, folding_item_text_style : folding_item_text_style})
+    }
+    else if(JSON.stringify(folding_item_text_style) !== JSON.stringify(this.state.folding_item_text_style)){
+      this.setState({folding_item_style : folding_item_style, folding_item_text_style : folding_item_text_style})
+    }
+
   }
 
   render()
   {
-
-    var style = copy_json(styles.FoldingItem);
-    console.log(this.props.visible)
-    if(this.props.visible === true)
-    {
-      console.log('true')
-      style.visibility = 'visible'
-    } else if (this.props.visible === false)
-    {
-      console.log('false')
-      style.visibility = 'hidden'
-    }
+    this.updateStyles();
 
     return (
-      <div className="folding-item" style={style}>
-        <p style={styles.folding_item_text}>{this.props.text}</p>
+      <div className="folding-item" style={this.state.folding_item_style}>
+        <p style={this.state.folding_item_text_style}>{this.props.text}</p>
       </div>
     )
 
